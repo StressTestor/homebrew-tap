5239a198577887c52c3683fba53c262519bcae1b
StressTestor <StressTestor@users.noreply.github.com>
2026-03-12 04:12:05 -0600

    agora 0.3.2

--- Casks/agora.rb
+++ Casks/agora.rb
@@ -1,8 +1,8 @@
 cask "agora" do
   arch arm: "aarch64"
 
-  version "0.3.1"
-  sha256 arm: "56bcf9a65f1a59677e439a1b41fb8539bb5441d1878790a10ade03de20e5cd1e"
+  version "0.3.2"
+  sha256 arm: "2fbe3e6ce0a36c3c2bf6774bcb6732634be29a1e23a93a13d618bab2a0cf8217"
 
   url "https://github.com/StressTestor/Agora-ai-agent-visualizer/releases/download/v#{version}/agora_#{version}_#{arch}.dmg"
   name "Agora"
